c24237c1270db5d6e6514d420b7a01408283f748
Louis Bima Susanto <69kamikazesusanto69@gmail.com>
2026-04-22 14:49:48 +0700

    fix: bug

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,17 +1,7 @@
-const nextConfig = {
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "briijcjcruapethbkrdp.supabase.co",
-        pathname: "/storage/v1/object/public/**",
-      },
-    ],
+const config = {
+  plugins: {
+    "@tailwindcss/postcss": {},
   },
 };
 
-export default nextConfig;
+export default config;
