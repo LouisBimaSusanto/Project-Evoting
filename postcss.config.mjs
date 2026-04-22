f7ea5850cad984bbebbaef8870b97c8002661605
Louis Bima Susanto <69kamikazesusanto69@gmail.com>
2026-04-22 14:39:12 +0700

    fix: supabase env vars

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,17 @@
-const config = {
-  plugins: {
-    "@tailwindcss/postcss": {},
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "briijcjcruapethbkrdp.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
   },
 };
 
-export default config;
+export default nextConfig;
